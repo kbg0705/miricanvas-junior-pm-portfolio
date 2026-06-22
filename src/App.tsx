38a90009feede31d6d4759390bbe2f0ba712f3d9
kbg0705 <kbg0705@ewha.ac.kr>
2026-06-22 11:35:07 +0900

    Set up routing with BrowserRouter in App componentUpdate src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,26 @@
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { HomePage } from './pages/HomePage';
+import { ProjectPage } from './pages/ProjectPage';
+
+function getBasename() {
+  return window.location.pathname.startsWith('/miricanvas-junior-pm-portfolio')
+    ? '/miricanvas-junior-pm-portfolio'
+    : '/';
+}
+
+function App() {
+  return (
+    <BrowserRouter basename={getBasename()}>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/projects/:projectId" element={<ProjectPage />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
 import { HomePage } from './pages/HomePage';
 
 function App() {
